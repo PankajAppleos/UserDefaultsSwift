30193de4d956aca6bb9115ac01a42584aae53af2
nirali28 <patelnirali287@yahoo.in>
2019-03-28 15:17:36 +0530

    new changes committed

--- UserDefaultsSwift/ViewController.swift
+++ UserDefaultsSwift/ViewController.swift
@@ -10,11 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    
+    @IBOutlet weak var btnclr: UIButton!
+    @IBOutlet weak var btnSave: UIButton!
+    @IBOutlet weak var txtlastname: UITextField!
+    @IBOutlet weak var txtfirstname: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
     }
-
-
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func btnSave(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        
+        defaults.set(txtfirstname.text, forKey: "firstName")
+        defaults.set(txtlastname.text, forKey: "lastName")
+        defaults.synchronize()
+        
+    print("fn=\(String(describing: txtfirstname.text)), ln=\(txtlastname.text ?? "nothing")")
+    }
+    
+   
+    
+    @IBAction func btnClear(_ sender: Any) {
+        if(txtfirstname.text == ""){
+            loadDefaults()
+            btnclr.setTitle("Clear", for: .normal)
+        }
+        else {
+            txtfirstname.text = ""
+            txtlastname.text = ""
+            btnclr.setTitle("Load", for: .normal)
+        }
+    }
+    func loadDefaults() {
+        let defaults = UserDefaults.standard
+        txtfirstname.text = defaults.object(forKey: "firstName") as? String
+        txtlastname.text = defaults.object(forKey: "lastName") as? String
+    }
+    
 }
 
